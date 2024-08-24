057b227b1e348aee1a69952eec1a13ecf52da576
awkrail <taichitary@gmail.com>
2024-08-24 14:34:34 +0900

    add ch07'2

--- haskell_world/ch07/Main.hs
+++ haskell_world/ch07/Main.hs
@@ -1,12 +1,24 @@
 import Shapes
 
-data Person = Person {
+data Vector a = Vector a a a deriving (Show)
+
+data Person = Person { 
     firstName :: String,
     lastName :: String,
-    age :: Int,
-    height :: Float,
-    phoneNumber :: String,
-    flavor :: String } deriving (Show)
+    age :: Int                   
+} deriving (Eq, Show)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+vplus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
+
+vmult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
 
 main = do
     print $ nudge (baseCircle 30) 10 20
