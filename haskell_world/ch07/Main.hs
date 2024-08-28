8e13d75c9d06972da090a2f5c5a8e4a42a887a31
awkrail <taichitary@gmail.com>
2024-08-28 15:15:26 +0900

    fixed

--- haskell_world/ch07/Main.hs
+++ haskell_world/ch07/Main.hs
@@ -17,7 +17,7 @@ type Name = String
 type PhoneBook = [(Name, PhoneNumber)]
 
 inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
-isPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
 
 data LockerState = Taken | Free deriving (Show, Eq)
 type Code = String
@@ -38,6 +38,76 @@ dotProd :: (Num a) => Vector a -> Vector a -> a
 
 vmult :: (Num a) => Vector a -> a -> Vector a
 (Vector i j k) `vmult` m = Vector (i*m) (j*m) (k*m)
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys      = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    |  x == a = Node x left right
+    |  x < a  = Node a (treeInsert x left) right
+    |  x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Green == Green = True
+    Yellow == Yellow = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _  = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing  = False
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _   = True
+
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
 
 main = do
     print $ nudge (baseCircle 30) 10 20
