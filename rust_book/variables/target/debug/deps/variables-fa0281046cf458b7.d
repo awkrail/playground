a07e556ac6fd43ea1b2feae2a8238a7648236317
awkrail <taichitary@gmail.com>
2024-09-08 15:56:01 +0900

    add variables

---
+++ rust_book/variables/target/debug/deps/variables-fa0281046cf458b7.d
@@ -0,0 +1,5 @@
+/Users/tainishi/playground/rust_book/variables/target/debug/deps/variables-fa0281046cf458b7: src/main.rs
+
+/Users/tainishi/playground/rust_book/variables/target/debug/deps/variables-fa0281046cf458b7.d: src/main.rs
+
+src/main.rs:
---
+++ rust_book/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/tainishi/playground/rust_book/variables/target/debug/variables: /Users/tainishi/playground/rust_book/variables/src/main.rs
