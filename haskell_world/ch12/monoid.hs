208b26733efa8d079d62451f79f49a51d862ee56
awkrail <taichitary@gmail.com>
2024-09-11 21:12:50 +0900

    monoid / foldMap tree structure

--- haskell_world/ch12/monoid.hs
+++ haskell_world/ch12/monoid.hs
@@ -1,4 +1,5 @@
 import Data.Monoid
+import qualified Data.Foldable as F
 
 lengthCompare :: String -> String -> Ordering
 lengthCompare x y = (length x `compare` length y) `mappend` (x `compare` y)
@@ -7,3 +8,22 @@ lengthCompare' x y = (length x `compare` length y) `mappend`
                      (vowels x `compare` vowels y) `mappend`
                      (x `compare` y)
     where vowels = length . filter (`elem` "aeiou")
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+instance F.Foldable Tree where
+    foldMap f EmptyTree = mempty
+    -- 畳み込みの順番を決める
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             F.foldMap f r `mappend`
+                             f x           
+
+testTree = Node 5
+            (Node 3
+                (Node 1 EmptyTree EmptyTree)
+                (Node 6 EmptyTree EmptyTree)
+            )
+            (Node 9
+                (Node 8 EmptyTree EmptyTree)
+                (Node 10 EmptyTree EmptyTree)
+            )
