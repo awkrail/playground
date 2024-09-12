b7a051db2babea3d4d507bd337a01f9697dd0541
awkrail <taichitary@gmail.com>
2024-09-12 09:56:10 +0900

    add monad

---
+++ haskell_world/ch13/monad.hs
@@ -0,0 +1,22 @@
+import Control.Arrow (ArrowChoice(right))
+applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+applyMaybe Nothing f = Nothing
+applyMaybe (Just x) f = f x
+
+type Birds = Int
+type Pole = (Birds, Birds)
+
+landLeft :: Birds -> Pole -> Maybe Pole
+landLeft n (left, right)
+    | abs ((left + n) - right) < 4 = Just (left + n, right)
+    | otherwise                    = Nothing
+
+landRight :: Birds -> Pole -> Maybe Pole
+landRight n (left, right)
+    | abs (left - (right + n)) < 4 = Just (left, right + n)
+    | otherwise                    = Nothing
+
+banana :: Pole -> Maybe Pole
+banana _ = Nothing
+
+x -: f = f x
